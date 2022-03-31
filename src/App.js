fe7fbefbc007a2ff277431bd1d2a6f2f712ff562
ameerhamzaexponento <ameer.hamza@exponento.com>
2022-04-01 00:53:16 +0500

    MUI Drawer implemented.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  AppBar,
+  Box,
+  CssBaseline,
+  Divider,
+  Drawer,
+  IconButton,
+  List,
+  ListItem, ListItemIcon, ListItemText,
+  Toolbar,
+  Typography, useTheme
+} from "@mui/material";
+
+import MenuIcon from '@mui/icons-material/Menu';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+
+
+import {useState} from "react";
+const App = () => {
+
+  const theme = useTheme();
+
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  }
+  const handleDrawerClose = () => {
+    setOpen(false);
+  }
+  return(
+      <>
+        <Box sx={{ display: 'flex' }}>
+          <CssBaseline />
+          <div className={`top-header ${open ? "active" : ""}`}>
+            <AppBar position="fixed" open={open}>
+              <Toolbar>
+                <IconButton
+                    color="inherit"
+                    aria-label="open drawer"
+                    onClick={handleDrawerOpen}
+                    edge="start"
+                    sx={{ mr: 2, ...(open && { display: 'none' }) }}
+                >
+                  <MenuIcon />
+                </IconButton>
+                <Typography variant="h6" noWrap component="div">
+                  Persistent drawer
+                </Typography>
+              </Toolbar>
+            </AppBar>
+          </div>
+          <Drawer
+              sx={{
+                width: "200px",
+                flexShrink: 0,
+                '& .MuiDrawer-paper': {
+                  width: "200px",
+                  boxSizing: 'border-box',
+                },
+              }}
+              variant="persistent"
+              anchor="left"
+              open={open}
+              className={`${open ? "active" : ""}`}
+          >
+            <div className="drawer-header">
+              <IconButton onClick={handleDrawerClose}>
+                {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+              </IconButton>
+            </div>
+            <Divider />
+            <List>
+              {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+                  <ListItem button key={text}>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItem>
+              ))}
+            </List>
+            <Divider />
+            <List>
+              {['All mail', 'Trash', 'Spam'].map((text, index) => (
+                  <ListItem button key={text}>
+                    <ListItemIcon>
+                      {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                    </ListItemIcon>
+                    <ListItemText primary={text} />
+                  </ListItem>
+              ))}
+            </List>
+          </Drawer>
+          <div className={`main ${open ? "active" : ""}`} open={open}>
+            <div className="drawer-header" />
+            <Typography paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus non
+              enim praesent elementum facilisis leo vel. Risus at ultrices mi tempus
+              imperdiet. Semper risus in hendrerit gravida rutrum quisque non tellus.
+              Convallis convallis tellus id interdum velit laoreet id donec ultrices.
+              Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+              adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra
+              nibh cras. Metus vulputate eu scelerisque felis imperdiet proin fermentum
+              leo. Mauris commodo quis imperdiet massa tincidunt. Cras tincidunt lobortis
+              feugiat vivamus at augue. At augue eget arcu dictum varius duis at
+              consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem donec massa
+              sapien faucibus et molestie ac.
+            </Typography>
+            <Typography paragraph>
+              Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper
+              eget nulla facilisi etiam dignissim diam. Pulvinar elementum integer enim
+              neque volutpat ac tincidunt. Ornare suspendisse sed nisi lacus sed viverra
+              tellus. Purus sit amet volutpat consequat mauris. Elementum eu facilisis
+              sed odio morbi. Euismod lacinia at quis risus sed vulputate odio. Morbi
+              tincidunt ornare massa eget egestas purus viverra accumsan in. In hendrerit
+              gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem
+              et tortor. Habitant morbi tristique senectus et. Adipiscing elit duis
+              tristique sollicitudin nibh sit. Ornare aenean euismod elementum nisi quis
+              eleifend. Commodo viverra maecenas accumsan lacus vel facilisis. Nulla
+              posuere sollicitudin aliquam ultrices sagittis orci a.
+            </Typography>
+          </div>
+        </Box>
+      </>
+  )
 }
-
-export default App;
+export default App
